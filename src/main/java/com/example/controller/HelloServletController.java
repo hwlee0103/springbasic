ad9ce49167cd1aaf51772689c10806708e1c8156
hwlee0103 <sdiverHyeWon0103@gmail.com>
2023-03-23 20:55:01 +0900

    230323 Spring Container, Context, Bean

--- src/main/java/com/example/controller/HelloServletController.java
+++ src/main/java/com/example/controller/HelloServletController.java
@@ -1,6 +1,5 @@
 package com.example.controller;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/hello")
 @Controller
 public class HelloServletController {
-
+    // view 페이지를 Return해 주지 않으므로 localhost로 띄웠을 때 404 에러 발생할 것
     @GetMapping("")
     public String hello(String name) {
         return "Hello World : " + name;
--- src/main/java/com/example/springbasic/SpringbasicDispatcherApplication.java
+++ src/main/java/com/example/springbasic/SpringbasicDispatcherApplication.java
@@ -14,7 +14,6 @@ import org.springframework.web.servlet.DispatcherServlet;
  * 내부의 Bean 이라고 하는 Object 를 구성해서 서버 어플리케이션으로 만들어줌
  */
 
-@Configuration
 public class SpringbasicDispatcherApplication {
 
     public static void main(String[] args) {
@@ -41,11 +40,9 @@ public class SpringbasicDispatcherApplication {
         };
 
         // 매핑되는 Bean/객체 를 등록 - class 정보를 주면 등록
-        // 스프링 컨테이너 초기화 작업, Template method 패턴으로 구성
-        // TODO: test
+        // 스프링 컨테이너 초기화 작업, Template method 패턴으로 구현
         applicationContext.registerBean(HelloServletController.class);
         applicationContext.refresh(); // 등록 시 항상 등록된 정보를 다시 불러들여야함
-
 
     }
 
